298427d06accacab01cc71b0717745176cbbecd4
vandadshahedy <shah.vandad@gmail.com>
2023-02-06 19:51:38 +0330

    folder

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    partial class supermario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(supermario));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.bg = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 5;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(150)))), ((int)(((byte)(255)))));
+            this.player.Image = global::WindowsFormsApp2.Properties.Resources.Untitled;
+            this.player.Location = new System.Drawing.Point(12, 325);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(48, 64);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // bg
+            // 
+            this.bg.BackColor = System.Drawing.SystemColors.Control;
+            this.bg.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.bg;
+            this.bg.Image = global::WindowsFormsApp2.Properties.Resources.bg;
+            this.bg.Location = new System.Drawing.Point(0, 0);
+            this.bg.Name = "bg";
+            this.bg.Size = new System.Drawing.Size(850, 450);
+            this.bg.TabIndex = 0;
+            this.bg.TabStop = false;
+            // 
+            // supermario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(150)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 448);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.bg);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "supermario";
+            this.Text = "super mario";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bg;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox player;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class supermario : Form
+    {
+        public void block(int X, int Y, int count = 1)
+        {
+            PictureBox newBlock = new PictureBox();
+            newBlock.Size = new Size(37 * count, 30);
+            newBlock.Location = new Point(X, Y);
+            newBlock.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.block;
+
+            Controls.Add(newBlock);
+
+            blocks.Add(newBlock);
+        }
+        public supermario()
+        {
+            InitializeComponent();
+        }
+        private List<PictureBox> blocks = new List<PictureBox>();
+
+        int hordir = 0;
+        int verdir = 0;
+        bool jumpf=true;
+        int speed = 5;
+        int maxjump =200;
+        private void keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                hordir = 1;
+            else if (e.KeyCode == Keys.Right)
+                hordir = -1;
+            if (e.KeyCode == Keys.Up)
+                verdir = -2;
+        }
+
+        private void keyup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.Left)
+                hordir = 0;
+
+            if (e.KeyCode == Keys.Up)
+                verdir = 0;
+        }
+        private void jump()
+        {
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                if (player.Top >= blocks[i].Bottom )
+                    maxjump = blocks[i].Bottom;
+            }
+            if (verdir == -2)
+            {
+                if (player.Top > maxjump && jumpf)
+                    verdir = -2;
+                else
+                {
+                    verdir = 2;
+                    jumpf = false;
+                }
+            }
+            else if (player.Top < 325)
+                verdir = 2;
+            if (player.Top < 325)
+                player.Top += verdir * speed;
+            else if (player.Top == 325 && verdir == -2)
+                player.Top += verdir * speed;
+        }
+        private void walk()
+        {
+            bg.Left += speed * hordir;
+            for (int i = 0; i < blocks.Count; i++)
+                    blocks[i].Left+= speed * hordir;
+
+            if (bg.Left <= 0)
+                bg.Width += bg.Width;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            jump();
+            walk();
+        }
+
+    }
+}
